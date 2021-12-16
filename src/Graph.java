dfda393a54bbd9823c10d76af60558cd61d6bbc0
Duda Ferreira <ferreira.mev@gmail.com>
2021-12-16 14:42:19 -0300

    Add Graph attributes/fields

---
+++ src/Graph.java
@@ -0,0 +1,11 @@
+import java.util.LinkedList;
+import java.util.ArrayList;
+
+public class Graph {
+    private int nNodes;
+    private ArrayList<ArrayList<Int>> adjMatrix;
+    private ArrayList<LinkedList<Int>> adjList;
+
+    
+
+}
