b3f2188a3b98c148a3ceedae5be0dffe17232438
Duda Ferreira <ferreira.mev@gmail.com>
2021-12-16 14:50:40 -0300

    Outline Graph constructor

--- src/Graph.java
+++ src/Graph.java
@@ -6,6 +6,29 @@ public class Graph {
     private ArrayList<ArrayList<Int>> adjMatrix;
     private ArrayList<LinkedList<Int>> adjList;
 
-    
+    public Graph(int nNodes, int reprChoice) {
+        this.nNodes = nNodes;
+
+        if (reprChoice == 0) {
+            this.adjList = null;
+
+            // Inicializa matriz de adjacência de zeros
+
+            for (int i=0; i < nNodes; i++) {
+
+                for (int j=0; j < nNodes; j++) {
+
+                }
+            }
+
+        } else if (reprChoice == 1) {
+            this.adjMatrix = null;
+
+            // Inicializa lista de adjacências sem vizinhos
+
+        } else {
+            throw new IllegalArgumentException("0 para representacao por matriz, 1 para grafo");
+        }
+    }
 
 }
